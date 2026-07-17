fa95cec4522ad34038d4a0d15a29559ea7c61a2d
Anatolii Subbotin <k-asubbot@c2r3p8.hive.fi>
2026-07-17 15:58:41 +0300

    conflicts resolved

--- pathfinder/pathfinderAlogrithm2.go
+++ pathfinder/pathfinderAlogrithm2.go
@@ -1,1 +1,27 @@
 package pathfinder
+<<<<<<< HEAD
+=======
+
+import (
+	m "trains/models"
+)
+
+func BFS(appData m.AppData) []string {
+	var visited map[string]struct{}
+	res := []string{}
+	start := appData.StartingStation.Name
+	end := appData.EndingStation.Name
+	posMax := 0
+	if len(appData.StartingStation.Connections) > len(appData.EndingStation.Connections) {
+		posMax = len(appData.EndingStation.Connections)
+	} else {
+		posMax = len(appData.StartingStation.Connections)
+	}
+
+	visited[start] = struct{}{}
+	for _, v := range appData.NetworkMap {
+
+	}
+	return res
+}
+>>>>>>> 504b1aa (algorithm 2 updated)
