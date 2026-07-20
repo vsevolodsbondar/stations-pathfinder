d8fabc1476d8363db1a5de083a168ba63e12d96c
Anatolii Subbotin <lotana2234@gmail.com>
2026-07-20 12:34:00 +0300

    Changed psth finder algorythm 2

--- pathfinder/pathfinderAlogrithm2.go
+++ pathfinder/pathfinderAlogrithm2.go
@@ -1,24 +1,109 @@
 package pathfinder
 
 import (
+	"fmt"
 	m "trains/models"
 )
 
-func BFS(appData m.AppData) []string {
-	var visited map[string]struct{}
-	res := []string{}
-	start := appData.StartingStation.Name
-	end := appData.EndingStation.Name
-	posMax := 0
-	if len(appData.StartingStation.Connections) > len(appData.EndingStation.Connections) {
-		posMax = len(appData.EndingStation.Connections)
-	} else {
-		posMax = len(appData.StartingStation.Connections)
+type Rail struct {
+	To       int
+	Capacity int
+	Flow     int
+	Reverse  int
+}
+
+type FlowGraph struct {
+	Rails [][]Rail
+}
+
+type FlowNetwork struct {
+	Graph       *FlowGraph
+	StationToID map[*m.Station]int
+}
+
+type Parent struct {
+	From int
+	Rail *Rail
+}
+
+func BuildFlowGraph(data *m.AppData) *FlowNetwork {
+	graph := &FlowGraph{
+		Rails: make([][]Rail, len(data.NetworkMap)*2),
+	}
+	stationToID := make(map[*m.Station]int)
+
+	id := 0
+	for _, station := range data.NetworkMap {
+		stationToID[station] = id
+		id += 2
+	}
+	for _, station := range data.NetworkMap {
+
+		in := stationToID[station]
+		out := in + 1
+		capacity := 1
+
+		if station == data.StartingStation || station == data.EndingStation {
+			capacity = data.TrainNumb
+		}
+
+		graph.AddRail(in, out, capacity)
+	}
+	for _, station := range data.NetworkMap {
+
+		fromOut := stationToID[station] + 1
+
+		for _, next := range station.Connections {
+
+			toIn := stationToID[next]
+
+			graph.AddRail(fromOut, toIn, 1)
+		}
+	}
+	return &FlowNetwork{
+		Graph:       graph,
+		StationToID: stationToID,
+	}
+}
+
+func (r Rail) RemainingCapacity() int {
+	return r.Capacity - r.Flow
+}
+
+func (g *FlowGraph) Print() {
+	for i, rails := range g.Rails {
+		fmt.Printf("%d:\n", i)
+		for _, rail := range rails {
+			fmt.Printf("->%d cap=%d flow=%d rev=%d\n", rail.To, rail.Capacity, rail.Flow, rail.Reverse)
+		}
+	}
+}
+
+func (g *FlowGraph) AddRail(from, to, capacity int) {
+	forwardIdx := len(g.Rails[from])
+	reverseIdx := len(g.Rails[to])
+	forward := Rail{
+		To:       to,
+		Capacity: capacity,
+		Flow:     0,
+		Reverse:  reverseIdx,
 	}
 
-	visited[start] = struct{}{}
-	for _, v := range appData.NetworkMap {
-
+	reverse := Rail{
+		To:       from,
+		Capacity: 0,
+		Flow:     0,
+		Reverse:  forwardIdx,
 	}
-	return res
+	g.Rails[from] = append(g.Rails[from], forward)
+	g.Rails[to] = append(g.Rails[to], reverse)
+}
+
+func (g *FlowGraph) BFS(start, end int) ([]Parent, bool) {
+	var parents = make([]Parent, len(g.Rails))
+	visited := make([]bool, len(g.Rails))
+	queue := []int{start}
+	current := queue[0]
+	queue = queue[1:]
+
 }
