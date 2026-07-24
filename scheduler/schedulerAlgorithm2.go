72b627f78d9f13aa2fff8aa9dd467bbd4dc7cc0b
Anatolii Subbotin <k-asubbot@c2r2p13.hive.fi>
2026-07-24 12:16:56 +0300

    scheduler algorithm 2: added move active trains function that advances all active trains by one station on their paths

--- scheduler/schedulerAlgorithm2.go
+++ scheduler/schedulerAlgorithm2.go
@@ -1,6 +1,7 @@
 package scheduler
 
 import (
+	"fmt"
 	m "trains/models"
 )
 
@@ -10,6 +11,9 @@ type Train struct {
 	Position int
 }
 
+// DistributeTrains assigns trains to the available paths so that the estimated
+// arrival time of the last train is minimized. Shorter paths receive more trains
+// because they become available again sooner.
 func DistributeTrains(paths [][]*m.Station, trainCount int) []int {
 	assigned := make([]int, len(paths))
 	arrival := make([]int, len(paths))
@@ -33,6 +37,9 @@ func DistributeTrains(paths [][]*m.Station, trainCount int) []int {
 	return assigned
 }
 
+// LaunchTrains starts new trains on their assigned paths whenever the first
+// station after the start is free. Newly launched trains are added to the list
+// of active trains and receive consecutive IDs.
 func LaunchTrains(
 	active *[]Train,
 	paths [][]*m.Station,
@@ -71,4 +78,72 @@ func LaunchTrains(
 		launched[pathID]++
 		(*nextID)++
 	}
+}
+
+// MoveTrains advances all active trains by one station if their next station
+// is available. It updates occupied stations, records train movements for the
+// current turn, and removes trains that have reached the destination.
+func MoveActiveTrains(
+	active *[]Train,
+	paths [][]*m.Station,
+	occupied map[*m.Station]bool,
+) []string {
+
+	for k := range occupied {
+		delete(occupied, k)
+	}
+
+	for _, train := range *active {
+		path := paths[train.PathID]
+		if train.Position < len(path)-1 {
+			cur := path[train.Position]
+			if cur != path[len(path)-1] {
+				occupied[cur] = true
+			}
+		}
+	}
+
+	moves := []string{}
+
+	for i := range *active {
+
+		train := &(*active)[i]
+		path := paths[train.PathID]
+
+		if train.Position >= len(path)-1 {
+			continue
+		}
+
+		current := path[train.Position]
+		next := path[train.Position+1]
+
+		if current != path[len(path)-1] {
+			delete(occupied, current)
+		}
+
+		if next != path[len(path)-1] {
+			if occupied[next] {
+				if current != path[len(path)-1] {
+					occupied[current] = true
+				}
+				continue
+			}
+			occupied[next] = true
+		}
+
+		train.Position++
+		moves = append(moves, fmt.Sprintf("T%d-%s", train.ID, next.Name))
+	}
+
+	newActive := (*active)[:0]
+
+	for _, train := range *active {
+		if train.Position < len(paths[train.PathID])-1 {
+			newActive = append(newActive, train)
+		}
+	}
+
+	*active = newActive
+
+	return moves
 }
