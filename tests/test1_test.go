d6be586f09df316c8659c32e7d0d5d5c437c4f72
admin <vsevolod.s.bondar@gmail.com>
2026-07-24 18:14:22 +0300

    first test case done.

---
+++ tests/test1_test.go
@@ -0,0 +1,71 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+	c "trains/cli"
+	m "trains/models"
+	p "trains/pathfinder"
+)
+
+func TestShouldFindMoreThanOneRouteAlgorithm1(t *testing.T) {
+	config := m.FlagConfig{
+		NetworkMapPath:  "./testData/waterloo.txt",
+		StartingStation: "waterloo",
+		EndingStation:   "st_pancras",
+		TrainNumb:       2,
+	}
+
+	appData, _ := c.DataConfiguration(config)
+
+	routes, _ := p.DFSRangedRouteSets(appData)
+	found2Routes := false
+	for _, v := range routes {
+		for _, r := range v {
+			r.PrintRoute()
+
+		}
+		if len(v) >= 2 {
+			found2Routes = true
+		}
+	}
+
+	if !found2Routes {
+		t.Errorf("Should find at least 2 routes.")
+	}
+
+}
+
+func TestShouldFindMoreThanOneRouteAlgorithm2(t *testing.T) {
+	config := m.FlagConfig{
+		NetworkMapPath:  "./testData/waterloo.txt",
+		StartingStation: "waterloo",
+		EndingStation:   "st_pancras",
+		TrainNumb:       2,
+	}
+
+	appData, _ := c.DataConfiguration(config)
+
+	res := p.BuildFlowGraph(&appData)
+	startID := res.StationToID[appData.StartingStation]
+	endID := res.StationToID[appData.EndingStation]
+
+	maxFlow := res.Graph.MaxFlow(startID, endID)
+	paths, _ := res.ExtractPaths(maxFlow)
+
+	found2Routes := false
+	for _, v := range paths {
+		route := ""
+		for _, st := range v {
+			route += st.Name + " "
+		}
+		fmt.Println(route)
+		if len(v) >= 2 {
+			found2Routes = true
+		}
+	}
+
+	if !found2Routes {
+		t.Errorf("Should find at least 2 routes.")
+	}
+}
